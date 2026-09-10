feace25e7bc69f4cd05a846784e9e76d2f8a9408
Arshia <arshiagharedaghi9@gmail.com>
2026-09-10 19:00:57 +0330

    create forms.py for user model

---
+++ user/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(UserCreationForm):
+     email = forms.EmailField()
+     class Meta:
+          model = User
+          fields = ('username' , 'password1' , 'password2' , 'email')
+
+class LoginForm(AuthenticationForm):
+     pass

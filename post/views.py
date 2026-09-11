e25020bac6208e7071c84ed8aef61aaf19afa275
Arshia <arshiagharedaghi9@gmail.com>
2026-09-11 12:29:20 +0330

    some changes in views.py

--- post/views.py
+++ post/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import post
 from django.contrib.auth.decorators import login_required
+from .forms import InputForm
 
 # Create New post
 @login_required
 def create_post(request):
-     if request.method == 'POST':
-          title = request.POST['title']
-          content = request.POST['content']
+     # if request.method == 'POST':
+     form = InputForm(request.POST or None)
+     if form.is_valid():
+               post_obj = form.save(commit=False)
+               post_obj.author = request.user
+               post_obj.save()
+               return redirect('all_posts')     
 
-          post.objects.create(title=title,content=content,author=request.user)
-          return redirect('all_posts')
-
-     return render(request, 'post/create.html')
+     return render(request, 'post/create.html' , {'form':form})
 
 # Read All posts
 def all_posts(request):
@@ -28,14 +30,14 @@ def post_details(request, pk):
 @login_required
 def update_post(request, pk):
      post_obj = get_object_or_404(post, pk=pk , author=request.user)
-     if request.method == 'POST':
-          post_obj.title = request.POST['title']
-          post_obj.content = request.POST['content']
-          post_obj.save()
+     
 
-          return redirect('post_details',pk=post_obj.pk)
+     form = InputForm(request.POST or None, instance=post_obj)     
+     if form.is_valid():
+         form.save()
+         return redirect('all_posts')
 
-     return render(request, 'post/update.html',{'post':post_obj})
+     return render(request, 'post/update.html',{'form':form})
 
 #Delete post
 @login_required

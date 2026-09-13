c315aaa476c8af7731287c422efe8034d5c96591
Arshia <arshiagharedaghi9@gmail.com>
2026-09-13 18:53:36 +0330

    create serializers.py for posts

---
+++ post/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import post
+
+class PostSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = post
+          fields = '__all__'
+          #just for showing - user can't sent it on the req
+          read_only_fields = ['author']

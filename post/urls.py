57b3d62982c50c366003349a003f5a201688d274
Arshia <arshiagharedaghi9@gmail.com>
2026-09-13 18:54:55 +0330

    create an api's for post and create functions for them

--- post/urls.py
+++ post/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path 
 from . import views
 
-urlpatterns = [ 
+
+urlpatterns = [
+     #HTML pages 
      path('', views.all_posts, name='all_posts'),
      path('create/',views.create_post,name='create_post'),
      path('<int:pk>/',views.post_details , name='post_details'),
      path('<int:pk>/update/',views.update_post,name='update_post'),
-     path('<int:pk>/delete/',views.delete_post,name='delete_post')
+     path('<int:pk>/delete/',views.delete_post,name='delete_post'),
+     
+     # API 
+     path('api/posts/',views.post_list_api,name='post_list_api'),
+     path('api/posts/<int:pk>/',views.post_detail_api,name='post_detail_api'),
+     path('api/posts/create/',views.post_create_api,name='post_create_api'),
+     path('api/posts/<int:pk>/update/',views.post_update_api,name='post_update_api'),
+     path('api/posts/<int:pk>/partial-update/',views.post_partial_update_api,name='post_partial_update_api'),
+     path('api/posts/<int:pk>/delete/',views.post_delete_api,name='post_delete_api'),
+     
 ]
--- post/views.py
+++ post/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import post
 from django.contrib.auth.decorators import login_required
 from .forms import InputForm
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+
 
 # Create New post
 @login_required
@@ -49,3 +55,83 @@ def delete_post(request, pk):
           return redirect('all_posts')
 
      return render(request,'post/delete.html',{'post':post_obj})
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def post_create_api(request):
+
+     serializer = PostSerializer(data=request.data)
+
+     if serializer.is_valid():
+          #jwt know who is this user !
+          serializer.save(author=request.user)
+
+          return Response(serializer.data)
+
+@api_view(['GET'])
+def post_list_api(request):
+
+     posts = post.objects.all()
+     serializer = PostSerializer(posts , many=True)
+
+     return Response(serializer.data)
+
+@api_view(['GET'])
+def post_detail_api(request, pk):
+
+    post_obj = get_object_or_404(post , pk=pk)
+
+    serializer = PostSerializer(post_obj)
+
+    return Response(serializer.data)
+# use put for changing all data of post 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def post_update_api(request, pk):
+
+    post_obj = get_object_or_404(post, pk=pk)
+
+    if post_obj.author != request.user:
+        return Response(
+         {"detail": "You do not have permission to edit this post."},
+         status=403
+     )
+
+    serializer = PostSerializer( post_obj , data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+# use patch for chaning just fileds you want 
+@api_view(['PATCH'])
+@permission_classes([IsAuthenticated])
+def post_partial_update_api(request, pk):
+
+    post_obj = get_object_or_404(post, pk=pk)
+
+    if post_obj.author != request.user:
+        return Response({"detail": "You do not have permission to edit this post."},status=403)
+
+    serializer = PostSerializer(post_obj,data=request.data,partial=True)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+    return Response(serializer.errors, status=400)
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def post_delete_api(request, pk):
+
+    post_obj = get_object_or_404(post, pk=pk)
+
+    if post_obj.author != request.user:
+        return Response(
+            {"detail": "You do not have permission to delete this post."},
+            status=403
+        )
+
+    post_obj.delete()
+
+    return Response({"message": "Post deleted successfully"})

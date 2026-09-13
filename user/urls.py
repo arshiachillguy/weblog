3dcf94e6321038f7ce740019387c8b566676d59e
Arshia <arshiagharedaghi9@gmail.com>
2026-09-13 18:57:16 +0330

    create jwt api's and some changes in db

--- user/urls.py
+++ user/urls.py
@@ -1,17 +1,24 @@
 from django.urls import path 
 from . import views
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView)
 
-urlpatterns = [ 
 
+urlpatterns = [     
+     # Authentication
      path('login/', views.login_page, name='login_page'),
      path("logout/", views.logout_view, name="logout"),
      path('register/', views.register_page, name='register_page'),
-     path('home/',views.home, name='home'),
      path('change-password',views.changePassword,name='changePassword'),
+
+     # Users
+     path('home/',views.home, name='home'),
      path('users/', views.all_users, name='all_users'),
      path('create/',views.create_user, name='create_user'),
      path('<int:pk>/', views.user_detail, name='user_detail'),
      path('<int:pk>/update/',views.update_user,name='update_user'),
      path('<int:pk>/delete/',views.delete_user,name='delete_user'),
-     path('home/',views.home, name='home')
+     
+     #JWT API 
+     path('api/token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
+     path('api/token/refresh/',TokenRefreshView.as_view(),name='token_refresh')
 ]
